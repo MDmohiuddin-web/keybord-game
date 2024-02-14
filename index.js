87fba65a8acaaa58ca7a2599670d6dec234f7c20
MDmohiuddin <149958768+MDmohiuddin-web@users.noreply.github.com>
2024-02-14 13:58:35 +0600

    add
    add

--- index.js
+++ index.js
@@ -1,71 +1,131 @@
-function continiue(){
-    let alpabet=getalpabet();
-    return alpabet;
+// function continiue(){
+//     let alphabet=getalpabet();
+//     console.log('get your alphabet',alphabet);
 
-}
-
-
-
-function play(){
-    let homesection=document.getElementById('home-screen');
-    homesection.classList.add('hidden');
+//     // display alphabet
+//     let carentalpabet=document.getElementById( 'current-alphabet');
+  
+//     carentalpabet.innerText=alphabet;
 
 
-let playgroundsection=document.getElementById('play-ground');
-playgroundsection.classList.remove('hidden')
+//     // set back ground color
 
-continiue()
-}
+//     setBackgroundColorById(alphabet);
+    
+// }
 
 
 
+// function play(){
+//     // let homesection=document.getElementById('home-screen');
+//     // homesection.classList.add('hidden');
 
-function getalpabet(){
-    let arraye=['A B C D E F G H I J K L M N O P Q R S T U V W X Y Z'];
-    let splite=arraye.split('');
-    console.log(splite);
-    let randomnum=Math.random() * 25;
-    let index=Math.round(randomnum);
-    console.log(index);
 
-    let alpabet=alpabets[index];
-    // console.log(index,alpabet);
-    return alpabet;
-}
+//     // let playgroundsection=document.getElementById('play-ground');
+//     // playgroundsection.classList.remove('hidden')
 
 
 
+//     hideElementById('home-screen');
+//     showElementById('play-ground');
 
+//     continiue()
+// }
 
+// function hideElementById(elementId) {
+//     const element = document.getElementById(elementId);
+//     element.classList.add('hidden');
+// }
 
-// let  resulatxt= document.getElementById("output");
-//     resulatxt.innerText=alphabetString ;
-
-
-
+// function showElementById(elementId) {
+//     const element = document.getElementById(elementId);
+//     element.classList.remove('hidden');
+// }
 
 
 
 
-// function continueGame() {
-//     let alphabet = getAlphabet();
+// function getalpabet(){
+//     let arrays='ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+//     let alphabets=arrays.split('');
+//     // console.log(alphabets);
+//     //random alphabet
+//     let random=Math.random() * 25;
+//     let index=Math.round(random);
+//     // console.log(index);
+
+//     let alphabet=alphabets[index];
+//     // console.log(index,alphabet);
 //     return alphabet;
 // }
 
-// function play() {
-//     let homeSection = document.getElementById('home-screen');
-//     homeSection.classList.add('hidden');
-
-//     let playgroundSection = document.getElementById('play-ground');
-//     playgroundSection.classList.remove('hidden');
-
-//     continueGame();
+// // setBackgroundColorById
+// function setBackgroundColorById(elementId){
+//     const element = document.getElementById(elementId);
+//     element.classList.add('bg-orange-400');
 // }
 
-// function getAlphabet() {
-//     const alphabetString = 'A B C D E F G H I J K L M N O P Q R S T U V W X Y Z';
-//     const alphabetArray = alphabetString.split(' ');
-//     const randomIndex = Math.floor(Math.random() * alphabetArray.length);
-//     const selectedAlphabet = alphabetArray[randomIndex];
-//     return selectedAlphabet;
+
+// main cod
+
+
+
+
+
+
+// // copy cod *******
+
+
+function continueGame(){
+    // step -1: generate a random alphabet
+    const alphabet = getARandomAlphabet();
+    console.log('your random alphabet', alphabet);
+
+    // set randomly generated alphabet to the screen (show it)
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    currentAlphabetElement.innerText = alphabet;
+
+    // set background color
+    setBackgroundColorById(alphabet);
+}
+
+function play(){
+    hideElementById('home-screen');
+    showElementById('play-ground');
+    continueGame();
+}
+
+
+// part 2
+function hideElementById(elementId) {
+    const element = document.getElementById(elementId);
+    element.classList.add('hidden');
+}
+
+function showElementById(elementId) {
+    const element = document.getElementById(elementId);
+    element.classList.remove('hidden');
+}
+
+function setBackgroundColorById(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.add('bg-orange-400');
+}
+
+// function removeBackgroundColorById(elementId){
+//     const element = document.getElementById(elementId);
+//     element.classList.remove('bg-orange-400');
 // }
+
+function getARandomAlphabet(){
+    const alphabetString = 'abcdefghijklmnopqrstuvwxyz';
+    const alphabets = alphabetString.split('');
+
+    const randomNumber = Math.random()*25;
+    const index = Math.round(randomNumber);
+
+    const alphabet = alphabets[index];
+    return alphabet;
+}
+// // part 2
+// // copy cod
